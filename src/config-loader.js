2bfa03d5ad556d629bf9fec571743a67700d1e02
Kael Zhang <i+github@kael.me>
2019-04-14 22:04:58 +0800

    #7: config: completes next and server config chain; fails tests

--- src/config-loader.js
+++ src/config-loader.js
@@ -1,12 +1,46 @@
 const fs = require('fs')
-const {isString} = require('core-util-is')
+const {isString, isFunction} = require('core-util-is')
 const hasOwnProperty = require('has-own-prop')
-const {extend} = require('next-compose-plugins')
+const {extend, withPlugins} = require('next-compose-plugins')
 
 const {error} = require('./error')
 const {getRawConfig} = require('./utils')
 
-const finderFactory = realpath => ({path: p}) => realpath === p
+const createFinder = realpath => ({path: p}) => realpath === p
+
+const createNextWithPlugins = config =>
+  (...args) => config
+    ? extend(config).withPlugins(...args)
+    : withPlugins(...args)
+
+// Usage
+// ```js
+// module.exports = (config, appInfo) => config
+// ```
+const reduceServerConfigs = chain => appInfo => {
+  const {length} = chain
+  const run = (serverConfig, i) => {
+    if (i === length) {
+      return serverConfig
+    }
+
+    const {
+      config,
+      configFile
+    } = chain[i]
+    if (!('server' in config)) {
+      return run(serverConfig, i + 1)
+    }
+
+    if (!isFunction(config.server)) {
+      throw error('INVALID_CONFIG_SERVER', configFile, config.server)
+    }
+
+    return run(config.server(serverConfig, appInfo), i + 1)
+  }
+
+  return run({}, 0)
+}
 
 module.exports = class ConfigLoader {
   constructor ({
@@ -16,7 +50,7 @@ module.exports = class ConfigLoader {
     this._server = server
     this._cwd = cwd
     this._paths = null
-    this._raw = []
+    this._chain = []
   }
 
   load () {
@@ -26,20 +60,20 @@ module.exports = class ConfigLoader {
     }) => {
       const rawConfig = getRawConfig(serverPath, configFileName)
       if (rawConfig) {
-        this._raw.push(rawConfig)
+        this._chain.push(rawConfig)
       }
     })
   }
 
   reload () {
-    this._raw.length = 0
+    this._chain.length = 0
     this.load()
   }
 
-  // We deferred the process of real configurations
+  // We deferred the process of merging configurations
   //////////////////////////////////////////////////////
   get plugins () {
-    return this._raw.reduce((plugins, {config}) => {
+    return this._chain.reduce((plugins, {config}) => {
       return config.plugins
         ? plugins.concat(config.plugins)
         : plugins
@@ -47,12 +81,36 @@ module.exports = class ConfigLoader {
   }
 
   get next () {
+    const nextConfig = this._chain.reduce((prev, {next}) => {
+      if (!next) {
+        return prev
+      }
 
+      if (!isFunction(next)) {
+        throw error('INVALID_CONFIG_NEXT', next)
+      }
+
+      // Usage
+      // ```js
+      // module.exports = withPlugins => withPlugins([...plugins], newConfig)
+      // ```
+      // withPlugins <- createNextWithPlugins(prev)
+      return next(createNextWithPlugins(prev))
+    })
+
+    if (!nextConfig) {
+      throw error('NEXT_CONFIG_NOT_FOUND')
+    }
+
+    // Dont allow webpack in nextConfig
+    delete nextConfig.webpack
+
+    return nextConfig
   }
 
-  // Returns ``
-  get server () {
 
+  get server () {
+    return reduceServerConfigs(this._chain)
   }
 
   // Returns `Function`
@@ -110,7 +168,7 @@ module.exports = class ConfigLoader {
 
       const realpath = fs.realpathSync(serverPath)
 
-      if (paths.findIndex(finderFactory(realpath)) === - 1) {
+      if (paths.findIndex(createFinder(realpath)) === - 1) {
         paths.unshift({
           serverPath: realpath,
           configFileName
--- src/error.js
+++ src/error.js
@@ -27,6 +27,12 @@ E('INVALID_SERVER_PATH', 'server.path must be a string, but got %s')
 
 E('INVALID_CONFIG_NAME', 'server.configFileName must be a string, but got %s')
 
+E('INVALID_CONFIG_NEXT', 'caviar.config.next must be a function, but got %s')
+
+E('NEXT_CONFIG_NOT_FOUND', 'next configuration not found, or please check the return value')
+
+E('INVALID_CONFIG_SERVER', 'caviar.config.server in "%s" must be a function, but got %s')
+
 module.exports = {
   error
 }
