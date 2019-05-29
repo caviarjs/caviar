1b198792b93a1536816f2092ff43000da00e0a58
Kael Zhang <i+github@kael.me>
2019-05-29 14:20:34 +0800

    block: WIP

--- src/DESIGN.md
+++ src/DESIGN.md
@@ -36,6 +36,7 @@ Some reusable and relatively insulated operation blocks need to work with each o
 
 - How to reuse babel configuration
 - How to support build phase
+- How to replace a block type of a binder with another block who has a similar interface.
 
 # Principle
 
---
+++ src/block/binder.js
@@ -0,0 +1,16 @@
+const Block = require('.')
+
+const {createError} = require('../error')
+// const {createSymbolFor} = require('../utils')
+
+const error = createError('BINDER')
+
+module.exports = class Orchestrator extends Block {
+  // constructor () {
+  //   super()
+  // }
+
+  _orchestrate () {
+
+  }
+}
