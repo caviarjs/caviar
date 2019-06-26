83db4479d8867b84514c74bb503064488f47d8d1
Kael Zhang <i+github@kael.me>
2019-06-26 18:17:11 +0800

    breaking: support phase: ready -> run(phase)

--- src/sandbox/spawner.js
+++ src/sandbox/spawner.js
@@ -4,7 +4,10 @@
 // `node /path/to/spawner/start.js ${optionsJSON}`
 const log = require('util').debuglog('caviar:spawner')
 
-const options = JSON.parse(process.argv[2])
+const {
+  phase,
+  ...options
+} = JSON.parse(process.argv[2])
 
 log('spawner env: %s', JSON.stringify(process.env, null, 2))
 
@@ -13,8 +16,8 @@ const {IS_NOT_SANDBOX_PLUGIN} = require('../constants')
 
 new Caviar(options)
 .applyPlugins(IS_NOT_SANDBOX_PLUGIN)
-.ready()
+.run(phase)
 .catch(err => {
-  console.error(`fails to start, reason:\n${err.stack}`)
+  console.error(`fails to run, phase:${phase}, reason:\n${err.stack}`)
   process.exit(1)
 })
